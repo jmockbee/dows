812b46f2e108cbf19c96d366c92d7436543242f4
Jon Mockbee <mockbee1000@gmail.com>
2019-07-27 11:40:48 -0400

    if else

--- src/com/JonMockbee/Main.java
+++ src/com/JonMockbee/Main.java
@@ -3,6 +3,25 @@ package com.JonMockbee;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+    }
+
+    private static void printDayOfTheWeek(int day) {
+        if (day == 0) {
+            System.out.println("sunday");
+        } else if (day = 1) {
+            System.out.println("monday");
+        } else if (day = 2) {
+            System.out.println("Tuesday");
+        } else if (day = 3) {
+            System.out.println("Wednesday");
+        } else if (day = 4) {
+            System.out.println("Thursday");
+        } else if (day = 5) {
+            System.out.println("Friday");
+        } else if (day = 6) {
+            System.out.println("Saturday");
+        }
     }
 }
